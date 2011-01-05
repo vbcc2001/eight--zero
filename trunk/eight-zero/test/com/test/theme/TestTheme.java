9bb6139983ed772a48b4b444ab44ee6db6074880
sz_Lee2010@163.com <sz_Lee2010@163.com@5cfcc167-3fb7-72a0-6f9d-15795b939fba>
2011-01-05 14:40:00 +0000

    增加了EncodingFilter方法

--- trunk/eight-zero/test/com/test/theme/TestTheme.java
+++ trunk/eight-zero/test/com/test/theme/TestTheme.java
@@ -3,9 +3,16 @@ package com.test.theme;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bbs.service.ThemeService;
 
 public class TestTheme {
-
+	
+	ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext_*.xml");
+	ThemeService themeService = (ThemeService)applicationContext.getBean("themeService");
+	
 	@Test
 	public void testDeleteTheme() {
 		fail("Not yet implemented");
